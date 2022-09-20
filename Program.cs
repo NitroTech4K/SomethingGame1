b3bfe208e12dd382fce0d1a2e371537cf979998b
NitroTech 4K <crtraer@gmail.com>
2022-09-20 10:43:42 -0300

    Added variables

--- Program.cs
+++ Program.cs
@@ -10,6 +10,39 @@ namespace SomethingGame1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("========SomethingGame========");
+
+            int Lives;
+            Lives = 3;
+
+            int Health;
+            Health = 100;
+
+            int Score;
+            Score = 0;
+
+            float ScoreMultipler;
+            ScoreMultipler = 2.5f;
+
+            int shield;
+            shield = 100;
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey(true);
         }
     }
 }
